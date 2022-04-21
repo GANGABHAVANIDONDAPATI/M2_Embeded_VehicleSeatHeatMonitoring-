78e9b82985e8324eb80d3db403097a4e2fe85ae7
GANGABHAVANIDONDAPATI <101049340+GANGABHAVANIDONDAPATI@users.noreply.github.com>
2022-04-21 17:31:18 +0530

    Add files via upload

---
+++ 3_Implementation/src/heat1.c
@@ -0,0 +1,132 @@
+<circuit reactStep="50" speed="1000000" noLinAcc="5" noLinStep="10" animate="0" type="simulide_0.1">
+ 
+Probe-61: 
+<item Show_id="false" rotation="-135" y="-188" id="Probe-61" PlotterCh="0" Show_volt="true" valLabely="0" objectName="Probe-61" valLabRot="45" labelrot="45" valLabelx="16" itemtype="Probe" labely="-16" x="-428" labelx="16" hflip="1" vflip="1"/>
+ 
+Probe-57: 
+<item Show_id="false" rotation="-45" y="-220" id="Probe-57" PlotterCh="0" Show_volt="true" valLabely="0" objectName="Probe-57" valLabRot="45" labelrot="45" valLabelx="16" itemtype="Probe" labely="-16" x="-388" labelx="16" hflip="1" vflip="1"/>
+ 
+Node-15: 
+<item Show_id="false" rotation="0" y="-84" id="Node-15" valLabely="0" objectName="Node-15" valLabRot="0" labelrot="0" valLabelx="0" itemtype="Node" labely="-24" x="-364" labelx="-16" hflip="1" vflip="1"/>
+ 
+Led-14: 
+<item x="-324" MaxCurrent="0.029999999999999999" valLabely="0" labely="-5" labelrot="-90" Threshold="2.3999999999999999" hflip="1" rotation="90" itemtype="Led" Resistance="0.59999999999999998" objectName="Led-14" Show_id="true" valLabRot="0" y="-172" vflip="1" labelx="8" Color="0" id="LED" Grounded="false" valLabelx="0"/>
+ 
+Node-13: 
+<item Show_id="false" rotation="0" y="-84" id="Node-13" valLabely="0" objectName="Node-13" valLabRot="0" labelrot="0" valLabelx="0" itemtype="Node" labely="-24" x="-388" labelx="-16" hflip="1" vflip="1"/>
+ 
+Node-12: 
+<item Show_id="false" rotation="0" y="-148" id="Node-12" valLabely="0" objectName="Node-12" valLabRot="0" labelrot="0" valLabelx="0" itemtype="Node" labely="-24" x="-364" labelx="-16" hflip="1" vflip="1"/>
+ 
+Switch-11: 
+<item x="-388" valLabely="0" labely="31" labelrot="-180" Norm_Close="false" hflip="1" rotation="-180" itemtype="Switch" objectName="Switch-11" Show_id="true" valLabRot="0" Poles="1" y="-148" vflip="1" labelx="8" id="heater SW" DT="false" valLabelx="0"/>
+ 
+Ground-10: 
+<item Show_id="false" rotation="0" y="-68" id="Ground-10" valLabely="0" objectName="Ground-10" valLabRot="0" labelrot="0" valLabelx="0" itemtype="Ground" labely="-24" x="-388" labelx="-16" hflip="1" vflip="1"/>
+ 
+Node-9: 
+<item Show_id="false" rotation="0" y="-364" id="Node-9" valLabely="0" objectName="Node-9" valLabRot="0" labelrot="0" valLabelx="0" itemtype="Node" labely="-24" x="-316" labelx="-16" hflip="1" vflip="1"/>
+ 
+Node-8: 
+<item Show_id="false" rotation="0" y="-436" id="Node-8" valLabely="0" objectName="Node-8" valLabRot="0" labelrot="0" valLabelx="0" itemtype="Node" labely="-24" x="-324" labelx="-16" hflip="1" vflip="1"/>
+ 
+Hd44780-7: 
+<item Show_id="true" rotation="0" y="-372" id="LCD DISPLAY" Cols="16" Rows="2" valLabely="0" objectName="Hd44780-7" valLabRot="0" labelrot="0" valLabelx="0" itemtype="Hd44780" labely="-82" x="-260" labelx="70" hflip="1" vflip="1"/>
+ 
+Ground-6: 
+<item Show_id="false" rotation="0" y="-420" id="Ground-6" valLabely="0" objectName="Ground-6" valLabRot="0" labelrot="0" valLabelx="0" itemtype="Ground" labely="-24" x="-308" labelx="-16" hflip="1" vflip="1"/>
+ 
+Probe-5: 
+<item Show_id="false" rotation="-45" y="-332" id="Probe-5" PlotterCh="0" Show_volt="true" valLabely="0" objectName="Probe-5" valLabRot="45" labelrot="45" valLabelx="16" itemtype="Probe" labely="-16" x="-364" labelx="16" hflip="1" vflip="1"/>
+ 
+Fixed Voltage-4: 
+<item x="-408" valLabely="8" labely="-24" Unit=" V" labelrot="0" hflip="1" rotation="0" itemtype="Fixed Voltage" Show_Volt="true" objectName="Fixed Voltage-4" Show_id="false" valLabRot="0" y="-376" Voltage="5" vflip="1" labelx="-64" id="Fixed Voltage-4" valLabelx="-16"/>
+ 
+Potentiometer-3: 
+<item x="-356" valLabely="-20" labely="-40" Unit=" kO" labelrot="0" Value_Ohm="200" hflip="1" rotation="0" itemtype="Potentiometer" Resistance="1" Show_res="true" objectName="Potentiometer-3" Show_id="true" valLabRot="0" y="-376" vflip="1" labelx="-16" id="knob" valLabelx="10"/>
+ 
+atmega328-2: 
+<item x="-428" valLabely="0" labely="-60" Ser_Monitor="false" labelrot="90" hflip="1" rotation="-90" itemtype="AVR" objectName="atmega328-2" Show_id="true" valLabRot="0" Program="D:/test embedded/SeatHeat/bin/Debug/SeatHeat.hex" Ser_Port="false" y="-264" vflip="1" labelx="22" id="atmega328-2" valLabelx="0" Mhz="16"/>
+ 
+Switch-1: 
+<item x="-428" valLabely="0" labely="18" labelrot="0" Norm_Close="false" hflip="1" rotation="0" itemtype="Switch" objectName="Switch-1" Show_id="true" valLabRot="0" Poles="1" y="-148" vflip="1" labelx="-14" id="Seat SW" DT="false" valLabelx="0"/>
+ 
+Connector-16: 
+<item x="-356" valLabely="0" labely="-24" labelrot="0" hflip="1" endpinid="atmega328-2-PC0" rotation="0" itemtype="Connector" objectName="Connector-16" Show_id="false" valLabRot="0" y="-360" enodeid="Circ_eNode-17" vflip="1" startpinid="Potentiometer-3-PinM" labelx="-16" id="Connector-16" valLabelx="0" pointList="-356,-360,-380,-360,-380,-300"/>
+ 
+Connector-18: 
+<item x="-392" valLabely="0" labely="-24" labelrot="0" hflip="1" endpinid="Potentiometer-3-PinA" rotation="0" itemtype="Connector" objectName="Connector-18" Show_id="false" valLabRot="0" y="-376" enodeid="Circ_eNode-19" vflip="1" startpinid="Fixed Voltage-4-outnod" labelx="-16" id="Connector-18" valLabelx="0" pointList="-392,-376,-372,-376,-372,-372"/>
+ 
+Connector-20: 
+<item x="-340" valLabely="0" labely="-24" labelrot="0" hflip="1" endpinid="Node-8-0" rotation="0" itemtype="Connector" objectName="Connector-20" Show_id="false" valLabRot="0" y="-376" enodeid="Circ_eNode-21" vflip="1" startpinid="Potentiometer-3-PinB" labelx="-16" id="Connector-20" valLabelx="0" pointList="-340,-376,-340,-436,-324,-436"/>
+ 
+Connector-24: 
+<item x="-236" valLabely="0" labely="-24" labelrot="0" hflip="1" endpinid="Node-9-0" rotation="0" itemtype="Connector" objectName="Connector-24" Show_id="false" valLabRot="0" y="-364" enodeid="Circ_eNode-21" vflip="1" startpinid="Hd44780-7-PinRW" labelx="-16" id="Connector-24" valLabelx="0" pointList="-236,-364,-316,-364"/>
+ 
+Connector-25: 
+<item x="-324" valLabely="0" labely="-24" labelrot="0" hflip="1" endpinid="Ground-6-Gnd" rotation="0" itemtype="Connector" objectName="Connector-25" Show_id="false" valLabRot="0" y="-436" enodeid="Circ_eNode-21" vflip="1" startpinid="Node-8-2" labelx="-16" id="Connector-25" valLabelx="0" pointList="-324,-436,-308,-436"/>
+ 
+Connector-26: 
+<item x="-316" valLabely="0" labely="-24" labelrot="0" hflip="1" endpinid="Node-9-1" rotation="0" itemtype="Connector" objectName="Connector-26" Show_id="false" valLabRot="0" y="-304" enodeid="Circ_eNode-21" vflip="1" startpinid="atmega328-2-PB1" labelx="-16" id="Connector-26" valLabelx="0" pointList="-316,-304,-316,-364"/>
+ 
+Connector-27: 
+<item x="-316" valLabely="0" labely="-24" labelrot="0" hflip="1" endpinid="Node-8-1" rotation="0" itemtype="Connector" objectName="Connector-27" Show_id="false" valLabRot="0" y="-364" enodeid="Circ_eNode-21" vflip="1" startpinid="Node-9-2" labelx="-16" id="Connector-27" valLabelx="0" pointList="-316,-364,-324,-364,-324,-436"/>
+ 
+Connector-28: 
+<item x="-228" valLabely="0" labely="-24" labelrot="0" hflip="1" endpinid="atmega328-2-PB3" rotation="0" itemtype="Connector" objectName="Connector-28" Show_id="false" valLabRot="0" y="-364" enodeid="Circ_eNode-29" vflip="1" startpinid="Hd44780-7-PinEn" labelx="-16" id="Connector-28" valLabelx="0" pointList="-228,-364,-228,-324,-332,-324,-332,-300"/>
+ 
+Connector-30: 
+<item x="-188" valLabely="0" labely="-24" labelrot="0" hflip="1" endpinid="atmega328-2-PB4" rotation="0" itemtype="Connector" objectName="Connector-30" Show_id="false" valLabRot="0" y="-364" enodeid="Circ_eNode-31" vflip="1" startpinid="Hd44780-7-dataPin4" labelx="-16" id="Connector-30" valLabelx="0" pointList="-188,-364,-188,-332,-340,-332,-340,-300"/>
+ 
+Connector-32: 
+<item x="-180" valLabely="0" labely="-24" labelrot="0" hflip="1" endpinid="atmega328-2-PB5" rotation="0" itemtype="Connector" objectName="Connector-32" Show_id="false" valLabRot="0" y="-364" enodeid="Circ_eNode-33" vflip="1" startpinid="Hd44780-7-dataPin5" labelx="-16" id="Connector-32" valLabelx="0" pointList="-180,-364,-180,-340,-348,-340,-348,-300"/>
+ 
+Connector-34: 
+<item x="-444" valLabely="0" labely="-24" labelrot="0" hflip="1" endpinid="Node-13-0" rotation="0" itemtype="Connector" objectName="Connector-34" Show_id="false" valLabRot="0" y="-148" enodeid="Circ_eNode-35" vflip="1" startpinid="Switch-1-pinP0" labelx="-16" id="Connector-34" valLabelx="0" pointList="-444,-148,-444,-84,-388,-84"/>
+ 
+Connector-36: 
+<item x="-372" valLabely="0" labely="-24" labelrot="0" hflip="1" endpinid="Node-12-0" rotation="0" itemtype="Connector" objectName="Connector-36" Show_id="false" valLabRot="0" y="-148" enodeid="Circ_eNode-35" vflip="1" startpinid="Switch-11-pinP0" labelx="-16" id="Connector-36" valLabelx="0" pointList="-372,-148,-364,-148"/>
+ 
+Connector-37: 
+<item x="-412" valLabely="0" labely="-24" labelrot="0" hflip="1" endpinid="atmega328-2-PD0" rotation="0" itemtype="Connector" objectName="Connector-37" Show_id="false" valLabRot="0" y="-148" enodeid="Circ_eNode-38" vflip="1" startpinid="Switch-1-switch0pinN" labelx="-16" id="Connector-37" valLabelx="0" pointList="-412,-148,-412,-252"/>
+ 
+Connector-41: 
+<item x="-364" valLabely="0" labely="-24" labelrot="0" hflip="1" endpinid="Switch-11-pinP0" rotation="0" itemtype="Connector" objectName="Connector-41" Show_id="false" valLabRot="0" y="-148" enodeid="Circ_eNode-35" vflip="1" startpinid="Node-12-2" labelx="-16" id="Connector-41" valLabelx="0" pointList="-364,-148,-372,-148"/>
+ 
+Connector-42: 
+<item x="-364" valLabely="0" labely="-24" labelrot="0" hflip="1" endpinid="Node-15-0" rotation="0" itemtype="Connector" objectName="Connector-42" Show_id="false" valLabRot="0" y="-148" enodeid="Circ_eNode-35" vflip="1" startpinid="Node-12-1" labelx="-16" id="Connector-42" valLabelx="0" pointList="-364,-148,-364,-84"/>
+ 
+Connector-43: 
+<item x="-388" valLabely="0" labely="-24" labelrot="0" hflip="1" endpinid="Ground-10-Gnd" rotation="0" itemtype="Connector" objectName="Connector-43" Show_id="false" valLabRot="0" y="-84" enodeid="Circ_eNode-35" vflip="1" startpinid="Node-13-2" labelx="-16" id="Connector-43" valLabelx="0" pointList="-388,-84,-388,-84"/>
+ 
+#include<avr/io.h>
+#include <util/delay.h>
+#include"Heat1.h"
+
+void Heat1()
+{
+    //input pin Port D pin 1
+    DDRD &= ~(1<<PD0); 
+    
+    //input pin Port D pin 2
+    DDRD &= ~(1<<PD1);
+
+    /*Giving logic HIGH for the input pins*/
+    PORTD |= (1<<PD0);
+    PORTD |= (1<<PD1);
+
+    /*Giving the output @ pin0 Port B */
+    DDRD |= (1<<PD2);
+
+    if  ( (!(PIND & (1<<PD0))) & (!(PIND & (1<<PD1))) )
+    {
+        //Turns on the LED if both the switches are turned on by the user
+        PORTD |= (1<<PD2);   
+        _delay_ms(1000);
+    }
+    else
+    {
+       PORTD &= ~(1<<PD2);  //Turns Off the LED
+        _delay_ms(1000);
+    }
+}
